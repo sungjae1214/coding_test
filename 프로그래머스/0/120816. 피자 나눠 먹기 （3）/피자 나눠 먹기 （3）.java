45c55565cae6136e10cf69d668714b202f3c1590
sungjae1214 <hong01026438204@gmail.com>
2025-05-14 22:54:36 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 85 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int slice, int n) {
+        int answer=0;
+        if(n%slice>=0.1)
+        answer = (n/slice)+1;
+        else answer = n/slice;
+        return answer;
+    }
+}
