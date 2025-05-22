2dc9c880cc1d5d52f862ffd5b069efa3d1e79e1b
sungjae1214 <hong01026438204@gmail.com>
2025-05-22 13:02:45 +0900

    [Bronze V] Title: ??!, Time: 220 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String A = sc.nextLine();
+        System.out.print(A+"??!");
+    }
+}
