8ce765815297a50b1f721da43f489087d5970067
sungjae1214 <hong01026438204@gmail.com>
2025-06-27 11:06:49 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 85.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int cnt = 0;
+        int cnt1 = 0;
+        int[] answer = {0,0};
+        for(int i = 0; i<num_list.length; i++){
+            if(num_list[i]%2==0) cnt++;
+            else cnt1++;
+        }
+        answer[0] = cnt;
+        answer[1] = cnt1;
+        return answer;
+    }
+}
