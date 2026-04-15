6108585dcc850af1af4b803d46243538f9cae6c5
sungjae1214 <hong01026438204@gmail.com>
2026-04-15 22:40:39 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.08 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int[] sides) {
+        int answer = 0;
+        
+        if(sides[0]<sides[1]) {
+            for(int i = sides[1]-sides[0]; i<sides[1]; i++){
+                answer++;
+            }
+            for(int i = sides[1]; i<(sides[0]+sides[1])-1; i++){
+                answer++;
+            }
+        }
+        else {
+            for(int i = sides[0]-sides[1]; i<sides[0]; i++){
+                answer++;
+            }
+            for(int i = sides[0]; i<(sides[0]+sides[1])-1; i++){
+                answer++;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
