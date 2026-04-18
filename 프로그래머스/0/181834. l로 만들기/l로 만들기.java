dd1261b3fe51089ac6fe140584e67065d44d256f
sungjae1214 <hong01026438204@gmail.com>
2026-04-18 23:47:36 +0900

    [level 0] Title: l로 만들기, Time: 1197.21 ms, Memory: 379 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        for(int i = 0; i<myString.length(); i++){
+            if('l'>myString.charAt(i)) answer +='l';
+            else answer += myString.charAt(i);
+        }
+        return answer;
+    }
+}
