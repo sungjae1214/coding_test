b3e157c6739c2986a260198d929cc0c11385522e
sungjae1214 <hong01026438204@gmail.com>
2026-04-20 09:41:40 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while(true){
+            if(n==0){
+                break;
+            }
+            answer += n%10;
+            n = n/10;
+        }
+            
+        return answer;
+    }
+}
